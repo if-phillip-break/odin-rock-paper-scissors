fa82201f7215c66cf2603990839e36ba1e3d8faa
if-phillip-break <Philtran2003@icloud.com>
2023-07-25 20:57:29 -0400

    Finished js aspect of rock paper scissors

--- javascript.js
+++ javascript.js
@@ -20,22 +20,49 @@ function playRound(playerSelection, computerSelection){
     let playerChoice = playerSelection.toLowerCase();
 
     if (playerChoice == "rock" && computerSelection == "Paper"){
-        return "You Lose! Paper beats Rock";
+        return 2;
     } else if (playerChoice == "rock" && computerSelection == "Scissors"){
-        return "You Win! Rock beats Scissors";
+        return 1;
     } else if (playerChoice == "scissors" && computerSelection == "Rock"){
-        return "You Lose! Rock beats Scissors";
+        return 2;
     } else if (playerChoice == "paper" && computerSelection == "Rock"){
-        return "You Win! Paper beats Rock";
+        return 1;
     } else if (playerChoice == "scissors" && computerSelection == "Paper"){
-        return "You Win! Scissors beats Paper";
+        return 1;
     } else if (playerChoice == "paper" && computerSelection == "Scissors"){
-        return "You Lose! Scissors beats Paper";
+        return 2;
     } else {
-        return "Tie! Try again";
+        return 0;
     }
 }
 
-const playerMove = "sciSsors";
-const computerMove = getComputerChoice();
-console.log(playRound(playerMove, computerMove));
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for (i = 0; i < 5; i++){
+
+        let outcome = playRound(prompt("Choose rock, paper, or scissors: "), getComputerChoice());
+
+        switch (outcome) {
+            case 1:
+                playerScore++;
+                break;
+            case 2:
+                computerScore++;
+                break;
+            default:
+        } 
+
+        console.log("You have won " + playerScore + " times, while the computer has won " + computerScore + " times.");
+    }
+
+    if (playerScore > computerScore) {
+        return "You win!";
+    } else if (playerScore < computerScore) {
+        return "You Lose!";
+    } else {
+        return "It's a tie!";
+    }
+}
+
+console.log(game())
