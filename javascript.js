2e837586f6a37ec4debec5ebddf1bb8febda1d9b
if-phillip-break <Philtran2003@icloud.com>
2023-08-10 09:17:45 -0400

    Finished rps

--- javascript.js
+++ javascript.js
@@ -36,61 +36,57 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
+const tally = document.querySelector('.tally');
+tally.style.cssText = "color: black";
+
+let playerScore = 0;
+let computerScore = 0;
+
 function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    const tally = document.querySelector('.tally');
-    tally.style.cssText = "color: black";
 
     let outcome;
 
     const paper = document.querySelector('.paper');
     paper.addEventListener('click', () => {
         outcome = playRound("paper", getComputerChoice());
-        switch (outcome) {
-            case 1:
-                playerScore++;
-                tally.textContent = "You: " + playerScore + " | Computer: " + computerScore;
-                break;
-            case 2:
-                computerScore++;
-                tally.textContent = "You: " + playerScore + " | Computer: " + computerScore;
-                break;
-            default:
-        } 
+        addPoint(outcome);
     })
     const rock = document.querySelector('.rock');
     rock.addEventListener('click', () => {
         outcome = playRound("rock", getComputerChoice());
-        switch (outcome) {
-            case 1:
-                playerScore++;
-                tally.textContent = "You: " + playerScore + " | Computer: " + computerScore;
-                break;
-            case 2:
-                computerScore++;
-                tally.textContent = "You: " + playerScore + " | Computer: " + computerScore;
-                break;
-            default:
-        } 
+        addPoint(outcome);
     })
     const scissors = document.querySelector('.scissors');
     scissors.addEventListener('click', () => {
         outcome = playRound("scissors", getComputerChoice());
-        switch (outcome) {
-            case 1:
-                playerScore++;
-                tally.textContent = "You: " + playerScore + " | Computer: " + computerScore;
-                break;
-            case 2:
-                computerScore++;
-                tally.textContent = "You: " + playerScore + " | Computer: " + computerScore;
-                break;
-            default:
-        } 
+        addPoint(outcome);
     })
 
+}
 
+function addPoint(outcome){
+    switch (outcome) {
+        case 1:
+            playerScore++;
+            updateTally();
+            checkOutcome();                
+            break;
+        case 2:
+            computerScore++;
+            updateTally();
+            checkOutcome();
+            break;
+        default:
+    } 
+}
+
+function updateTally(){
+    tally.textContent = "You: " + playerScore + " | Computer: " + computerScore;
+}
+
+const body = document.querySelector("body");
+
+function checkOutcome(){
     if (playerScore === 5 || computerScore === 5){
 
         if (playerScore > computerScore) {
@@ -101,6 +97,7 @@ function game(){
             tally.textContent =  "It's a tie!";
         }
     }
+
 }
 
 game();
